eae7534e6a20072dbd3cab6e532d5134a79eeddd
萧飒歌 <icocoabeans@163.com>
2021-04-24 17:57:39 +0800

    fix version

--- src/ulogcorn/__init__.py
+++ src/ulogcorn/__init__.py
@@ -1,5 +1,5 @@
 from ulogcorn.logger import UnifyHandler, StubbedGunicornLogger, StandaloneApplication
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 __all__ = ["StubbedGunicornLogger", "UnifyHandler", "StandaloneApplication", "__version__"]
