7b006f40fb69dd768be3d7c2a92b8c01d308b939
Joy <icocoabeans@gmail.com>
2022-07-26 15:47:46 +0800

    update dependencies

--- src/ulogcorn/__init__.py
+++ src/ulogcorn/__init__.py
@@ -1,5 +1,5 @@
 from ulogcorn.logger import UnifyHandler, StubbedGunicornLogger, StandaloneApplication, UvicornHandler
 
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 
 __all__ = ["StubbedGunicornLogger", "UnifyHandler", "StandaloneApplication", "UvicornHandler", "__version__"]
